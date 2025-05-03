4956e3b604e9b95037274a3cc78d0b1020506866
Benni13 <benjaminbbiringer@icloud.comn>
2025-05-03 11:14:22 +0200

    Verbindung zur erstellten Testdatenbank

---
+++ Test_Datenbanken.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+#Verbindung zur lokalen Datenbank
+
+conn = psycopg2.connect(
+    dbname="test_aktien",
+    user="postgres",
+    password="Benjamin13",
+    host="localhost",
+    port="5432"
+)
+
+print("Verbindung erfolgreich")
+conn.close()
+
+
+
+
